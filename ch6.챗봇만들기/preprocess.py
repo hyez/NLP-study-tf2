4ca35347955ae53ecad14b0d0d73c9401a53bd39
HYEZ <oohyejioo@gmail.com>
2021-01-10 23:56:31 +0900

    전처리

--- ch6.챗봇만들기/preprocess.py
+++ ch6.챗봇만들기/preprocess.py
@@ -24,10 +24,11 @@ CHANGE_FILTER = re.compile(FILTERS)
 
 MAX_SEQUENCES = 25 # 최대 문장 길이
 
+
 # 데이터 불러오는 함수
 def load_data(path):
     data_df = pd.read_csv(path, header=0)
-    question, answer = list(data_df['Q'], list(data_df['A']))
+    question, answer = list(data_df['Q']), list(data_df['A'])
 
     return question, answer
 
@@ -56,10 +57,10 @@ def prepor_like_morphlized(data):
 # 단어 사전을 만드는 함수
 def load_vocabulary(path, vocab_path, tokenize_as_morph=False):
     vocabulary_list = []
-    if not os.path.exist(vocab_path):
+    if not os.path.exists(vocab_path):
         # 이미 생성된 사전 파일이 존재하지 않으므로 데이터를 가지고 만들어야 한다.
         # 그래서 데이터가 존재 하면 사전을 만들기 위해서 데이터 파일의 존재 유무를 확인한다.
-        if os.path.exist(path):
+        if os.path.exists(path):
             data_df = pd.read_csv(path, encoding='utf-8')
             question, answer = list(data_df['Q']), list(data_df['A'])
             data = []
@@ -75,7 +76,7 @@ def load_vocabulary(path, vocab_path, tokenize_as_morph=False):
             for word in words:
                 vocabulary_file.write(word + '\n')
     
-    with open(vocab_path, 'r', encding='utf-8') as vocabulary_file:
+    with open(vocab_path, 'r', encoding='utf-8') as vocabulary_file:
         for line in vocabulary_file:
             vocabulary_list.append(line.strip())
 
@@ -92,7 +93,7 @@ def make_vocabulary(vocabulary_list):
 
 
 # 인코더에 대한 전처리 (인코더에 적용될 입력값을 만드는 함수)
-def enc_processing(value, dictionary):
+def enc_processing(value, dictionary, tokenize_as_morph=False):
     sequences_input_index = []
     sequences_length = []
 
@@ -102,9 +103,9 @@ def enc_processing(value, dictionary):
         
         for word in sequence.split():
             if dictionary.get(word) is not None: # 단어 사전에 있으면 단어 인덱스
-                sequence_index.extend([dictionary[word]])
+                sequence_idx.extend([dictionary[word]])
             else: # 없으면 UNK 인덱스
-                sequence_index.extend([dictionary[UNK]])
+                sequence_idx.extend([dictionary[UNK]])
 
         if len(sequence_idx) > MAX_SEQUENCES:
             sequence_idx = sequence_idx[:MAX_SEQUENCES] # 최대 문장 길이보다 길면 잘라줌
@@ -141,20 +142,33 @@ def dec_output_processing(value, dictionary, tokenize_as_morph=False):
     return np.asarray(sequences_output_index), sequences_length
 
 # 디코더 타깃값을 만드는 함수
+
 def dec_target_processing(value, dictionary, tokenize_as_morph=False):
+    # 인덱스 값들을 가지고 있는
+    # 배열이다.(누적된다)
     sequences_target_index = []
-
+    # 형태소 토크나이징 사용 유무
+    if tokenize_as_morph:
+        value = prepro_like_morphlized(value)
+    # 한줄씩 불어온다.
     for sequence in value:
-        sequence = re.sub(CHANGE_FILTER, '', sequence)
-        sequence_idx = [dictionary[word] for word in sequence.split()]
-
-        # 종료 토큰 넣어주기
-        if len(sequence_idx) >= MAX_SEQUENCES:
-            sequence_idx = sequence_idx[:MAX_SEQUENCES - 1] + [dictionary[END]]
+        # FILTERS = "([~.,!?\"':;)(])"
+        # 정규화를 사용하여 필터에 들어 있는
+        # 값들을 "" 으로 치환 한다.
+        sequence = re.sub(CHANGE_FILTER, "", sequence)
+        # 문장에서 스페이스 단위별로 단어를 가져와서
+        # 딕셔너리의 값인 인덱스를 넣어 준다.
+        # 디코딩 출력의 마지막에 END를 넣어 준다.
+        sequence_index = [dictionary[word] if word in dictionary else dictionary[UNK] for word in sequence.split()]
+        # 문장 제한 길이보다 길어질 경우 뒤에 토큰을 자르고 있다.
+        # 그리고 END 토큰을 넣어 준다
+        if len(sequence_index) >= MAX_SEQUENCES:
+            sequence_index = sequence_index[:MAX_SEQUENCES - 1] + [dictionary[END]]
         else:
-            sequence_idx += dictionary[END]
-        
-        sequence_idx += (MAX_SEQUENCES - len(sequence_idx)) * [dictionary[PAD]]
-        sequences_target_index.append(sequence_idx)
-
+            sequence_index += [dictionary[END]]
+        # max_sequence_length보다 문장 길이가 작다면 빈 부분에 PAD(0)를 넣어준다.
+        sequence_index += (MAX_SEQUENCES - len(sequence_index)) * [dictionary[PAD]]
+        # 인덱스화 되어 있는 값을 sequences_target_index에 넣어 준다.
+        sequences_target_index.append(sequence_index)
     return np.asarray(sequences_target_index)
+
